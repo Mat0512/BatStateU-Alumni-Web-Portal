c3beabbfa86a63fa1ad5f67bae721f5a6df739c6
Mathew Mendoza <mathewmendoza0512@gmail.com>
2022-11-27 11:46:13 +0800

    added new entry on cors and env var on axios client domain param

--- back_end/configs/corsAllowList.js
+++ back_end/configs/corsAllowList.js
@@ -1,3 +1,7 @@
-const corsAllowList = ["http://localhost:3000", "http://localhost:4000"];
+const corsAllowList = [
+    "http://localhost:3000",
+    "http://localhost:4000",
+    "https://test-app-7mcje.ondigitalocean.app",
+];
 
 module.exports = corsAllowList;
