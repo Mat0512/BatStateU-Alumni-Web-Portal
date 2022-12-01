94bbd5d6fd0c8fc76594cd7f76bb4d4ca33091c7
Mathew Mendoza <mathewmendoza0512@gmail.com>
2022-12-01 17:16:46 +0800

    update style login

--- front_end/src/login/Login.js
+++ front_end/src/login/Login.js
@@ -3,7 +3,7 @@ import { Banner } from "./Banner";
 
 const Login = ({ admin }) => {
     return (
-        <div className="w-screen h-full p-2 sm:px-2 bg-white-100 flex justify-center items-center overflow-y-auto">
+        <div className="w-screen h-full sm:h-screen p-2 sm:px-2 bg-white-100 flex justify-center items-center overflow-y-auto">
             <div className="sm:max-w-2xl sm:h-96 shadow-lg shadow-grey-200 flex flex-col sm:flex-row">
                 <LoginForm adminLogin={admin ? true : false} />
                 <Banner adminLogin={admin ? true : false} />
